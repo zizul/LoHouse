2bca5e3e9e618072c9aec8f5345c578ede15afb5
Kacper Zdanowicz <kacperz@man.poznan.pl>
2024-10-09 20:17:26 +0200

    Initial commit

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,51 @@
+//
+//  DragAndDrop.cs
+//
+//  Created by Rigoberto Sáenz Imbacuán (https://linkedin.com/in/rsaenzi/)
+//  Copyright © 2021. All rights reserved.
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+
+public class DragAndDrop : MonoBehaviour
+{
+
+    // The plane the object is currently being dragged on
+    Plane dragPlane;
+
+    // The difference between where the mouse is on the drag plane and 
+    // where the origin of the object is on the drag plane
+    Vector3 offset;
+
+    Camera myMainCamera;
+
+    void Start()
+    {
+        myMainCamera = Camera.main;
+    }
+
+    void OnMouseDown()
+    {
+
+        dragPlane = new Plane(myMainCamera.transform.forward, transform.position);
+        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition);
+
+        float planeDist;
+        dragPlane.Raycast(camRay, out planeDist);
+        offset = transform.position - camRay.GetPoint(planeDist);
+    }
+
+    void OnMouseDrag()
+    {
+
+        Ray camRay = myMainCamera.ScreenPointToRay(Input.mousePosition);
+
+        float planeDist;
+        dragPlane.Raycast(camRay, out planeDist);
+        transform.position = camRay.GetPoint(planeDist) + offset;
+    }
+}
